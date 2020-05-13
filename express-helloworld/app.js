008093102c69df6644ddcc0c2bbff7669b96e362
rafaelrliscano <rafaelrliscano@gmail.com>
2020-05-13 16:04:08 -0500

    add rafa - aura context

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
-app.listen(8080, function () {
+app.get('/rafa', function (req, res) {
+  res.send('Hello rafa!\n');
+});
+
+app.get('/aura', function (req, res) {
+  res.send('Hello pli!\n');
+});
+
+app.listen(8081, function () {
   console.log('Example app listening on port 8080!');
 });
 
