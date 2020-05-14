b957854de2c8624b7da7f52eaca743e1d19c94f5
rafaelrliscano <rafaelrliscano@gmail.com>
2020-05-14 09:54:07 -0500

    Ajustes

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,11 +9,7 @@ app.get('/rafa', function (req, res) {
   res.send('Hello rafa!\n');
 });
 
-app.get('/aura', function (req, res) {
-  res.send('Hello pli!\n');
-});
-
-app.listen(8081, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
 
